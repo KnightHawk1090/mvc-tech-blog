37321688ec4e8f2a39a95752179896a28cfbfdf1
KnightHawk1090 <121210332+KnightHawk1090@users.noreply.github.com>
2023-05-12 21:52:51 -0500

    updated comment-routes file

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -12,3 +12,19 @@ router.get('/', (req, res) => {
         res.status(500).json(err);
     });
 });
+
+// create a new comment. if error occurs during process return 400 error
+router.post('/', withAuth, (req, res) => {
+    if(req.session) {
+        Comment.create({
+            comment_text: req.body.comment_text,
+            post_id: req.body.post_id,
+            user_id: req.session.user_id
+        })
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    }
+});
